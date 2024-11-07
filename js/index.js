48da6c0c3f7b6f4be2ec3dea2be45d4e9536dadd
ray-yepez <1001.30255498.ucla@gmail.com>
2024-11-06 20:07:36 -0400

    (feature): first commit

--- js/index.js
+++ js/index.js
@@ -28,3 +28,47 @@ alert(`Valen más de $500: ${JSON.stringify(valenMasDe(500))}`);
 
 // Valen más de 300
 alert(`Valen más de $300: ${JSON.stringify(valenMasDe(300))}`);
+
+
+let agregarArticulo = (art) => {
+  Dt_articulos.push(art);
+  return "Artículo agregado: " + JSON.stringify(art);
+}
+ alert(agregarArticulo({id:6,nombre:"Chicha",costo: 500}));
+ alert(JSON.stringify(articulosSuPrecio()));
+
+ let meterArticulo = (art) => {
+  Dt_articulos.forEach((obj) => {
+    obj.id++;
+  });
+  Dt_articulos.unshift(art);
+  return "Articulo añadido: " + JSON.stringify(art);
+ }
+
+ alert(meterArticulo({id:1,nombre:"Malta", costo: 800}));
+ alert(JSON.stringify(articulosSuPrecio()));
+
+ let  quitarUlt = () =>  "Se eliminó" + JSON.stringify(Dt_articulos.pop());
+ 
+
+ alert(quitarUlt());
+ alert(JSON.stringify(articulosSuPrecio()));
+
+ let quitarPrim = () => {
+  Dt_articulos.forEach(art => {
+    art.id--;
+  });
+  return "Se eliminó " + JSON.stringify(Dt_articulos.shift());
+ }
+
+ alert(quitarPrim());
+ alert(JSON.stringify(articulosSuPrecio()));
+
+ let quitar = (pos) => {
+  return "Se eliminó: " + JSON.stringify(Dt_articulos.splice(pos));
+ }
+
+ alert(quitar(3));
+ alert(JSON.stringify(articulosSuPrecio()));
+
+alert(JSON.stringify(Dt_articulos.reverse()));
